966d2b9548fbc5a726496c5ee2f7ebd7ed807c87
moritz@wolter.tech <moritz@wolter.tech>
2022-06-15 10:08:29 +0200

    packet 2d code.

--- src/jaxwt/packets.py
+++ src/jaxwt/packets.py
@@ -5,13 +5,14 @@
 # Copyright (c) 2020 Moritz Wolter
 #
 import collections
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, List, Optional, Tuple, Dict, Union
 
 import jax
 import jax.numpy as jnp
 import pywt
 
 from .conv_fwt import _fwt_pad, _get_filter_arrays
+from .conv_fwt_2d import wavedec2
 from .utils import Wavelet
 
 if TYPE_CHECKING:
@@ -23,19 +24,33 @@ else:
 class WaveletPacket(BaseDict):
     """A wavelet packet tree."""
 
-    def __init__(self, data: jnp.ndarray, wavelet: Wavelet, mode: str = "reflect"):
+    def __init__(self, data: jnp.ndarray, wavelet: Wavelet, mode: str = "reflect",
+                 max_level: Optional[int] = None):
         """Create a wavelet packet decomposition object.
 
         Args:
-            data (jnp.array): The input data array of shape [time].
+            data (jnp.array): The input data array of shape [batch_size, time].
             wavelet (Wavelet): The wavelet used for the decomposition.
             mode (str): The desired padding method. Choose i.e.
-                "reflect", "symmetric" or "zero".
+                "reflect", "symmetric" or "zero". Defaults to "reflect".
         """
-        self.input_data = jnp.expand_dims(jnp.expand_dims(data, 0), 0)
+        if len(data.shape) == 1:
+            self.input_data = jnp.expand_dims(jnp.expand_dims(data, 0), 0)
+        elif len(data.shape) == 2:
+            self.input_data = jnp.expand_dims(data, 1)
+
         self.wavelet = wavelet
         self.mode = mode
-        self._wavepacketdec(self.input_data, wavelet)
+        self.data = {}
+
+        dec_lo, dec_hi, _, _ = _get_filter_arrays(wavelet, flip=True)
+        filt_len = dec_lo.shape[-1]
+        filt = jnp.stack([dec_lo, dec_hi], 0)
+
+        if max_level is None:
+            max_level = pywt.dwt_max_level(data.shape[-1], filt_len)
+        self._recursive_dwt(self.input_data, filt, level=0, max_level=max_level, path="")
+
 
     def get_level(self, level: int) -> List[str]:
         """Return the graycodes for a given level.
@@ -82,17 +97,70 @@ class WaveletPacket(BaseDict):
         else:
             self.data[path] = jnp.squeeze(data)
 
-    def _wavepacketdec(
+
+
+
+class WaveletPacket2D(BaseDict):
+    """A wavelet packet tree."""
+
+    def __init__(self, data: jnp.ndarray, wavelet: Wavelet, mode: str = "reflect",
+                 max_level: Optional[int] = None):
+        """Create a 2D-wavelet packet decomposition object.
+
+        Args:
+            data (jnp.array): The input data array of shape [batch_size, height, width].
+            wavelet (Wavelet): The wavelet used for the decomposition.
+            mode (str): The desired padding method. Choose i.e.
+                "reflect", "symmetric" or "zero". Defaults to "reflect".
+        """
+        self.input_data = data
+        self.wavelet = wavelet
+        self.mode = mode
+        self.data = {}
+        self.max_level = max_level
+        if max_level is None:
+            self.max_level = pywt.dwt_max_level(min(self.input_data.shape[-2:]),
+                                                self.wavelet.dec_len)
+        self._recursive_dwt2d(self.input_data, level=0, path="")
+
+
+    def get_level(self, level: int) -> List[str]:
+        """Return the graycodes for a given level.
+
+        Args:
+            level (int): The required depth of the tree.
+
+        Returns:
+            list: A list with the node names.
+        """
+        return self._get_graycode_order(level)
+
+    def _get_graycode_order(self, level: int, x: str = "a", y: str = "d") -> List[str]:
+        graycode_order = [x, y]
+        for _ in range(level - 1):
+            graycode_order = [x + path for path in graycode_order] + [
+                y + path for path in graycode_order[::-1]
+            ]
+        return graycode_order
+
+    def _recursive_dwt2d(
         self,
         data: jnp.ndarray,
-        wavelet: Wavelet,
-        level: Optional[int] = None,
+        level: int,
+        path: str,
     ) -> None:
-        self.data = {}
-        dec_lo, dec_hi, _, _ = _get_filter_arrays(wavelet, flip=True)
-        filt_len = dec_lo.shape[-1]
-        filt = jnp.stack([dec_lo, dec_hi], 0)
+        self.data[path] = jnp.squeeze(data)
+        if level < self.max_level:
+            result_a, (result_h, result_v, result_d) = wavedec2(
+              data, self.wavelet, 1, mode=self.mode 
+            )
+            # assert for type checking
+            assert not isinstance(result_a, tuple)
+            self._recursive_dwt2d(result_a, level + 1, path + "a")
+            self._recursive_dwt2d(result_h, level + 1, path + "h")
+            self._recursive_dwt2d(result_v, level + 1, path + "v")
+            self._recursive_dwt2d(result_d, level + 1, path + "d")
+        else:
+            self.data[path] = jnp.squeeze(data) # TODO: remove squeeze ?
 
-        if level is None:
-            level = pywt.dwt_max_level(data.shape[-1], filt_len)
-        self._recursive_dwt(data, filt, level=0, max_level=level, path="")
+
--- tests/test_packets.py
+++ tests/test_packets.py
@@ -4,34 +4,61 @@
 # Copyright (c) 2020 Moritz Wolter
 #
 
+from itertools import product
+
 import jax.numpy as np
 import numpy as nnp
 import pytest
 import pywt
 
-from src.jaxwt._lorenz import generate_lorenz
-from src.jaxwt.packets import WaveletPacket
+from src.jaxwt.packets import WaveletPacket, WaveletPacket2D
 
 
+# TODO: add more input shapes. and level none
+@pytest.mark.parametrize("input_shape", ((2,256), (3,255)))
 @pytest.mark.parametrize("wavelet", ("haar", "db2", "db3"))
 @pytest.mark.parametrize("level", (2, 4))
 @pytest.mark.parametrize("mode", ("reflect", "symmetric", "zero"))
-def test_packets_lorenz(wavelet, level, mode):
-    """Test wavelet analysis and synthesis on lorenz signal."""
+def test_packets_1d(input_shape, wavelet, level, mode):
+    """Test the wavelet packet analysis code."""
     wavelet = pywt.Wavelet(wavelet)
-    lorenz = generate_lorenz(tmax=0.99)[:, 0].astype(np.float64)
-    jwp = WaveletPacket(lorenz, wavelet, mode=mode)
+    input_data = np.array(nnp.random.randn(*input_shape))
+    jwp = WaveletPacket(input_data, wavelet, mode=mode)
     nodes = jwp.get_level(level)
     jnp_lst = []
     for node in nodes:
         jnp_lst.append(jwp[node])
     jres = np.stack(jnp_lst)
 
-    np_lorenz = nnp.array(lorenz._value)
-    wp = pywt.WaveletPacket(np_lorenz, wavelet=wavelet, mode=mode)
+    np_input_data = nnp.array(input_data._value)
+    wp = pywt.WaveletPacket(np_input_data, wavelet=wavelet, mode=mode)
     nodes = [node.path for node in wp.get_level(level, "freq")]
     np_lst = []
     for node in nodes:
         np_lst.append(wp[node].data)
+    res = np.stack(np_lst)
+    assert np.allclose(jres, res)
+
+
+@pytest.mark.parametrize("input_shape", ((2,32,32), (3,33,33)))
+@pytest.mark.parametrize("wavelet", ("haar", "db2"))
+@pytest.mark.parametrize("level", (2, 3))
+@pytest.mark.parametrize("mode", ("reflect", "symmetric", "zero"))
+def test_packets_2d(input_shape, wavelet, level, mode):
+    """Test the wavelet packet analysis code."""
+    wavelet = pywt.Wavelet(wavelet)
+    input_data = np.array(nnp.random.randn(*input_shape))
+    jwp = WaveletPacket2D(input_data, wavelet, mode=mode)
+    jnp_list = []
+    wp_keys = list(product(["a", "h", "v", "d"], repeat=level))
+    for node in wp_keys:
+        jnp_list.append(jwp["".join(node)])
+    jres = np.stack(jnp_list)
+
+    np_input_data = nnp.array(input_data._value)
+    wp = pywt.WaveletPacket2D(np_input_data, wavelet=wavelet, mode=mode)
+    np_lst = []
+    for node in wp_keys:
+        np_lst.append(wp["".join(node)].data)
     res = np.stack(np_lst)
     assert np.allclose(jres, res)
